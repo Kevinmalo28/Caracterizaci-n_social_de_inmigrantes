bf172a9c41d4cc11a25f8f625ebbeebbd2e14cdc
Kevin Macías <92650610+Kevinmalo28@users.noreply.github.com>
2022-06-08 00:52:28 -0500

    Update Consulta #3.sql

--- Consulta #3.sql
+++ Consulta #3.sql
@@ -1,4 +1,4 @@
-**Mostrar histórico entre el tiempo de arribo al país y el tiempo en acercarse al departamento de migración. En una columna debe aparecer los nombres y apellidos, en otra columna la fecha de arribo y en otra columna la fecha de acercamiento a migración 
+--Mostrar histórico entre el tiempo de arribo al país y el tiempo en acercarse al departamento de migración. En una columna debe aparecer los nombres y apellidos, en otra columna la fecha de arribo y en otra columna la fecha de acercamiento a migración 
 
 SELECT CONCAT(DATOS_INMIGRANTE.NOMBRES,' ',DATOS_INMIGRANTE.APELLIDOS) NOMBRES_Y_APELLIDOS,FECHA_INGRESO, FECHA_REGISTRO 
 	FROM REGISTRO_MIGRACION
