1b7891ca5eb0e00ffe67c612cff3f711691a23d3
Kevin Macías <92650610+Kevinmalo28@users.noreply.github.com>
2022-06-07 23:53:15 -0500

    Add files via upload

---
+++ Consulta #4.sql
@@ -0,0 +1,8 @@
+**Un listado de los objetivos de arribo al país más comunes, en una columna debe aparecer los nombre y apellidos, en otra columna debe aparecer el objetivo de arribo y en otra la nacionalidad
+
+
+SELECT CONCAT(DATOS_INMIGRANTE.NOMBRES,' ',DATOS_INMIGRANTE.APELLIDOS) NOMBRES_Y_APELLIDOS, OBJETIVO_ARRIBO, DATOS_INMIGRANTE.NACIONALIDAD 
+	FROM REGISTRO_MIGRACION
+	INNER JOIN DATOS_INMIGRANTE
+	ON REGISTRO_MIGRACION.ID_DATOS_INMIGRANTE = DATOS_INMIGRANTE.ID_DATOS_INMIGRANTE
+ORDER BY OBJETIVO_ARRIBO ASC;
