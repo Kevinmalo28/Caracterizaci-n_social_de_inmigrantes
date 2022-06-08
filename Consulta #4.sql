e02571fb8bce264b45d10e23362de5d49fe77452
Kevin Macías <92650610+Kevinmalo28@users.noreply.github.com>
2022-06-08 00:52:45 -0500

    Update Consulta #4.sql

--- Consulta #4.sql
+++ Consulta #4.sql
@@ -1,4 +1,4 @@
-**Un listado de los objetivos de arribo al país más comunes, en una columna debe aparecer los nombre y apellidos, en otra columna debe aparecer el objetivo de arribo y en otra la nacionalidad
+--Un listado de los objetivos de arribo al país más comunes, en una columna debe aparecer los nombre y apellidos, en otra columna debe aparecer el objetivo de arribo y en otra la nacionalidad
 
 
 SELECT CONCAT(DATOS_INMIGRANTE.NOMBRES,' ',DATOS_INMIGRANTE.APELLIDOS) NOMBRES_Y_APELLIDOS, OBJETIVO_ARRIBO, DATOS_INMIGRANTE.NACIONALIDAD 
